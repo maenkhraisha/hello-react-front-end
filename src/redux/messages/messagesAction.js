aff7b2436342abd7292a862183bf6fa9dce9a003
maen <maenkh@yahoo.com>
2022-10-29 23:15:54 +0300

    show the message

---
+++ src/redux/messages/messagesAction.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const GET_MESSAGES = 'GET_MESSAGES';
+
+const getMessages = createAsyncThunk(
+  GET_MESSAGES,
+  async () => {
+    const response = await fetch('http://127.0.0.1:3000/api/v1/messages');
+    console.log(response);
+    const data = await response.json();
+
+    return data.data;
+  },
+
+);
+
+export default getMessages;
